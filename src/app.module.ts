6f298de12065c0859c095bc03964047c0c668436
Leonardo Augusto <l.dos.santos.soares@avanade.com>
2022-03-07 14:46:41 -0300

    Project Microservice in MongoDB

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-main', {
+    autoCreate: true
+  }),
+    ProductModule
+],
+  controllers: [AppController, ProductController],
+  providers: [AppService],
+})
+export class AppModule {}
