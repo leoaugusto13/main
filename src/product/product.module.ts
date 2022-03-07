72fa84dac0a91bc5f45a76cba01b4c7a05573b22
Leonardo Augusto <l.dos.santos.soares@avanade.com>
2022-03-07 15:02:21 -0300

    Add app.module and update create product.module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { ProductSchema } from './product.model';
 
 @Module({
-  providers: [ProductService]
+    imports: [
+        MongooseModule.forFeature([{name: 'Products', schema: ProductSchema}])
+    ],
+    controllers: [ProductController],
+    providers: [ProductService]
 })
 export class ProductModule {}
