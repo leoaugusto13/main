64a543517cb0a6f853e7f70c827973917d1d11a7
Leonardo Augusto <l.dos.santos.soares@avanade.com>
2022-03-07 15:24:12 -0300

    Alter product.model and product.module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
-import { MongooseModule, Schema } from '@nestjs/mongoose';
-import { ProductSchema } from './product.model';
+import { MongooseModule} from '@nestjs/mongoose';
+import { Product, ProductSchema } from './product.model';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{name: 'Products', schema: ProductSchema}])
+        MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}])
     ],
     controllers: [ProductController],
     providers: [ProductService]
